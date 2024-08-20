a40cf46200a934c457ab5b28f59453279e463e99
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2024-08-20 20:13:08 -0300

    feat: adding new exceptions

--- src/main/java/com/propertymanager/demo/infra/security/exceptioins/ErrorHandler.java
+++ src/main/java/com/propertymanager/demo/infra/security/exceptioins/ErrorHandler.java
@@ -1,11 +1,18 @@
 package com.propertymanager.demo.infra.security.exceptioins;
 
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.nio.file.AccessDeniedException;
 
 @RestControllerAdvice
 public class ErrorHandler {
@@ -24,6 +31,31 @@ public class ErrorHandler {
     @ExceptionHandler(ValidateException.class)
     public ResponseEntity treatErrorBusinessRule(ValidateException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity handleHttpMessageNotReadable(HttpMessageNotReadableException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity handleConstraintViolation(ConstraintViolationException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity handleAccressDenied() {
+        return ResponseEntity.badRequest().body("Acesso negado");
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity handleDataIntegrityViolation(DataIntegrityViolationException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity handleHttpRequestMethodNotSupported() {
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body("Método HTTP não suportado.");
     }
 
     public record ExeptionData(String field, String message) {
